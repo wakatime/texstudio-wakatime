bf1e4200570f85b5e7a8aa1e1469ef42b06927e6
Alan Hamlett <alan.hamlett@gmail.com>
2019-07-16 07:20:03 -0700

    ability to install without cloning repo, and ability to re-install without duplicating macro

--- install.py
+++ install.py
@@ -9,10 +9,13 @@
 
 
 import os
-import glob
 import json
 import platform
 import sys
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
 
 
 PY2 = (sys.version_info[0] == 2)
@@ -36,14 +39,15 @@ if PY2:
 
 def main():
     macro = build_macro()
-    save_macro_file(macro)
+    path = find_macro_file()
+    save_macro_file(macro, path)
     print('Installed. You may now run TeXstudio.')
     if platform.system() == 'Windows':
         input('Press [Enter] to exit...')
 
 
 def build_macro():
-    """Build the contents that adds wakatime.txsMacro file."""
+    """Build the WakaTime macro object from source JavaScript files."""
 
     script = {
         "abbrev": '',
@@ -58,35 +62,48 @@ def build_macro():
     script['tag'] = []
     script['tag'].append('%SCRIPT')
     for filename in FILES:
-        path = get_file_path(filename)
-        with open(path) as f:
-            line = f.readline()
-            while line:
-                line = line.rstrip()
-                script['tag'].append(line)
-                line = f.readline()
+        contents = get_file_contents(filename)
+        if not contents:
+            continue
+        for line in contents.splitlines():
+            script['tag'].append(line.rstrip())
     return script
 
 
-def get_file_path(filename):
+def get_file_contents(filename):
     """Get file contents from local clone or GitHub repo."""
 
     if os.path.exists(os.path.join(SRC_DIR, filename)):
-        return os.path.join(SRC_DIR, filename)
+        with open(os.path.join(SRC_DIR, filename)) as fh:
+            return fh.read()
     else:
-        return ROOT_URL + filename
+        url = ROOT_URL + filename
+        resp = urlopen(url)
+        return resp.read()
 
 
-def save_macro_file(macro):
-    """Writes Macro_X.txsMacro file."""
+def find_macro_file():
+    """Returns path of existing installed macro or an unused macro number file."""
 
-    os.chdir(CONFIG_DIR)
-    total_files = len(glob.glob1(CONFIG_DIR, 'Macro_*.txsMacro'))
-    print('Found {} file(s) from macro folder.'.format(total_files))
+    number = 0
+    while True:
+        filename = 'Macro_{}.txsMacro'.format(number)
+        macroFile = os.path.join(CONFIG_DIR, filename)
+        if not os.path.exists(macroFile):
+            print('Installing WakaTime macro {}.'.format(macroFile))
+            return macroFile
+        with open(macroFile, 'r', encoding='utf-8') as fh:
+            macro = json.load(fh)
+        if macro.get('name') == 'WakaTime':
+            print('Found existing WakaTime macro {}.'.format(macroFile))
+            return macroFile
+        number += 1
 
-    macroFile = os.path.join(CONFIG_DIR, 'Macro_{}.txsMacro'.format(total_files))
 
-    with open(macroFile, 'w', encoding='utf-8') as fh:
+def save_macro_file(macro, path):
+    """Writes macro contents to txsMacro path."""
+
+    with open(path, 'w', encoding='utf-8') as fh:
         json.dump(macro, fh, sort_keys=True, indent=2)
 
 
